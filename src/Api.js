01bd7168b4ab0d42b3f93082ec87a5720b2eef73
oo11boy <editkhone@gmail.com>
2023-08-17 15:56:42 +0430

    api

--- src/Api.js
+++ src/Api.js
@@ -1,2 +1,7 @@
-export let Api="http://localhost:3000"
-export let domain="http://localhost:3001"
+export let Api="https://sheypoorapi.vercel.app"
+export let domain="https://sheypoor.vercel.app"
+
+
+// api=https://sheypoorapi.vercel.app v http://localhost:3000
+
+// domain=https://sheypoor.vercel.app v http://localhost:3001
